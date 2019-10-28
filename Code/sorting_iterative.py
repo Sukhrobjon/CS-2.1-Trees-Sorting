02d73a3ebab0ca3695ad9ce0ca5b7e0677759e34
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-10-28 11:38:44 -0700

    [implemented] is_sorted

--- Code/sorting_iterative.py
+++ Code/sorting_iterative.py
@@ -7,6 +7,17 @@ def is_sorted(items):
     TODO: Memory usage: ??? Why and under what conditions?"""
     # TODO: Check that all adjacent items are in order, return early if so
 
+    i = 0
+    j = 1
+
+    while j <= len(items) - 1:
+        if items[i] > items[j]:  # found unsorted pair
+            return False
+        i += 1
+        j += 1
+
+    return True
+
 
 def bubble_sort(items):
     """Sort given items by swapping adjacent items that are out of order, and
@@ -35,3 +46,9 @@ def insertion_sort(items):
     # TODO: Repeat until all items are in sorted order
     # TODO: Take first unsorted item
     # TODO: Insert it in sorted order in front of items
+
+
+if __name__ == "__main__":
+    items = [1, 2, 3, 4]
+    un_items = [1, 2, 6, 5]
+    print(is_sorted(un_items))
