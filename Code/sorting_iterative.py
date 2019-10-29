604520b5d15f7f2bda2c20e327529602c81e466d
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-10-29 14:26:59 -0700

    [finished] selection sort

--- Code/sorting_iterative.py
+++ Code/sorting_iterative.py
@@ -29,13 +29,28 @@ def bubble_sort(items):
 
 
 def selection_sort(items):
-    """Sort given items by finding minimum item, swapping it with first
+    """
+    Sort given items by finding minimum item, swapping it with first
     unsorted item, and repeating until all items are in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Repeat until all items are in sorted order
-    # TODO: Find minimum item in unsorted items
-    # TODO: Swap it with first unsorted item
+    Running time: Best, average and worst time is O(n^2)
+    Memory usage: O(1) It is allocated memory for variables. we are not
+    using any data structure in the memory.
+    NOTE: This is the worst sorting algorithm. Use Insertion instead.
+    Args:
+        items(list): list of unsorted elements
+    Return:
+        items(list): sorted list inplace
+    """
+    for i in range(len(items)):
+        # last sorted elements index in the array
+        min_idx = i
+        # rest of right side of the unsorted array
+        for j in range(i+1, len(items)):
+            if items[j] < items[i]:
+                min_idx = j
+        items[i], items[min_idx] = items[min_idx], items[i]
+
+    return items
 
 
 def insertion_sort(items):
@@ -48,7 +63,8 @@ def insertion_sort(items):
     # TODO: Insert it in sorted order in front of items
 
 
+
 if __name__ == "__main__":
     items = [1, 2, 3, 4]
-    un_items = [1, 2, 6, 5]
-    print(is_sorted(un_items))
+    un_items = [3, 2, 6, 5]
+    print(selection_sort(un_items))
